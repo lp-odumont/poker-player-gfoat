87eca0dde14a501ee6ed6f91f971363922e6a806
Markus Wedler <wedler@synopsys.com>
2021-11-08 11:34:56 +0100

    raise on king and ace

--- player.py
+++ player.py
@@ -3,6 +3,11 @@ class Player:
     VERSION = "Greatest Folder of all times v0.0.1"
 
     def betRequest(self, game_state):
+        own_card = game_state['players'][game_state['in_action']]["hole_cards"]
+        for x in own_card:
+            if x['rank'] in ['K','A']:
+                game_state['current_buy_in']*2 - game_state['players'][game_state['in_action']] ['bet']
+            
         return game_state['current_buy_in'] - game_state['players'][game_state['in_action']] ['bet']
 
     def showdown(self, game_state):
