32cf9cd8a6ba58408ba03dccd3394cbaea7f08dc
Markus Wedler <wedler@synopsys.com>
2021-11-08 11:12:49 +0100

    allways call

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Greatest Folder of all times v0.0.1"
 
     def betRequest(self, game_state):
-        return 0
+        return game_state['current_buy_in'] - game_state['players'][game_state['in_action']] ['bet']
 
     def showdown(self, game_state):
         pass
