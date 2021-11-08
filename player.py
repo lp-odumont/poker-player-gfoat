8bfe4b420aea703a7cf9dc8bb48128bbab3c5f1f
Markus Wedler <wedler@synopsys.com>
2021-11-08 11:44:39 +0100

    raise more on doubles

--- player.py
+++ player.py
@@ -1,9 +1,14 @@
+def is_double(own_cards):
+    return  own_cards[0]['rank'] == own_cards[1]['rank']
 
 class Player:
-    VERSION = "Greatest Folder of all times v0.0.2"
+    VERSION = "Greatest Folder of all times v0.0.3"
 
     def betRequest(self, game_state):
         own_card = game_state['players'][game_state['in_action']]["hole_cards"]
+        if is_double(own_card):
+            return game_state['current_buy_in']*4 - game_state['players'][game_state['in_action']] ['bet']
+
         for x in own_card:
             if x['rank'] in ['K','A']:
                 game_state['current_buy_in']*2 - game_state['players'][game_state['in_action']] ['bet']
