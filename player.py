229d128714d6387b08561e1c89c01e5e0b9f3105
Markus Wedler <wedler@synopsys.com>
2021-11-08 11:57:11 +0100

    tripple detection

--- player.py
+++ player.py
@@ -1,11 +1,23 @@
 def is_double(own_cards):
     return  own_cards[0]['rank'] == own_cards[1]['rank']
 
+def has_tripple(own_cards,community_cards):
+    if not is_double(own_cards):
+        return False
+    
+    for x in community_cards:
+        if x['rank'] == own_cards[0]['rank']:
+            return True
+    return False
+
 class Player:
-    VERSION = "Greatest Folder of all times v0.0.3"
+    VERSION = "Greatest Folder of all times v0.0.4"
 
     def betRequest(self, game_state):
         own_card = game_state['players'][game_state['in_action']]["hole_cards"]
+        community_cards = game_state['community_cards']
+        if has_tripple(own_card, community_cards):
+            return game_state['current_buy_in']*8 - game_state['players'][game_state['in_action']] ['bet']
         if is_double(own_card):
             return game_state['current_buy_in']*4 - game_state['players'][game_state['in_action']] ['bet']
 
