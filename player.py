5ad5297ecedfccf61ecd792741d731f3ba2d35df
Olivier Dumont <odumont@synopsys.com>
2021-11-08 13:27:42 +0100

    reworked to new API

--- player.py
+++ player.py
@@ -1,14 +1,3 @@
-def is_double(own_cards):
-    return  own_cards[0]['rank'] == own_cards[1]['rank']
-
-def has_tripple(own_cards,community_cards):
-    if not is_double(own_cards):
-        return False
-    
-    for x in community_cards:
-        if x['rank'] == own_cards[0]['rank']:
-            return True
-    return False
 
 class Player:
     VERSION = "Greatest Folder of all times v0.0.4"
@@ -16,9 +5,9 @@ class Player:
     def betRequest(self, game_state):
         own_card = game_state['players'][game_state['in_action']]["hole_cards"]
         community_cards = game_state['community_cards']
-        if has_tripple(own_card, community_cards):
+        if self.is_three(own_card + community_cards):
             return game_state['current_buy_in']*8 - game_state['players'][game_state['in_action']] ['bet']
-        if is_double(own_card):
+        if self.is_pair(own_card):
             return game_state['current_buy_in']*4 - game_state['players'][game_state['in_action']] ['bet']
 
         for x in own_card:
