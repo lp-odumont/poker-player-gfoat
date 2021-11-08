6b2b99230f5120763e3070478e0bd5cb486f7c0f
Olivier Dumont <odumont@synopsys.com>
2021-11-08 14:44:30 +0100

    computing strength of hands

--- player.py
+++ player.py
@@ -1,29 +1,44 @@
 
 class Player:
-    VERSION = "Greatest Folder of all times v0.0.5"
+    VERSION = "Greatest Folder of all times v0.0.7"
 
     def betRequest(self, game_state):
         own_card = game_state['players'][game_state['in_action']]["hole_cards"]
         community_cards = game_state['community_cards']
-        if self.is_three(own_card + community_cards):
-            return game_state['current_buy_in']*8 - game_state['players'][game_state['in_action']] ['bet']
-        if self.is_pair(own_card):
-            return game_state['current_buy_in']*4 - game_state['players'][game_state['in_action']] ['bet']
+        the_bet_factor=self.compute_bet(own_card,community_cards)
+        return (game_state['current_buy_in'] - game_state['players'][game_state['in_action']] ['bet'])*the_bet_factor
 
-        for x in own_card:
-            if x['rank'] in ['K','A']:
-                return game_state['current_buy_in']*2 - game_state['players'][game_state['in_action']] ['bet']
-
-        if self.is_garbage(own_card, community_cards):
-            return 0
-
-        return game_state['current_buy_in'] - game_state['players'][game_state['in_action']] ['bet']
+      
 
     def showdown(self, game_state):
         pass
 
+    def strength(self, hand):
+        if self.is_four(hand):
+            return 5
+        if self.is_three(hand):
+            return 4
+        if self.is_two_pairs(hand):
+            return 3
+        if  self.is_pair(hand):
+            return 2
+        if self.has_kings_aces(hand):
+            return 1
+        return 0
+    
+    def compute_bet(self,own_hand,community_hand):
+        rel_strength = self.strength(own_hand+community_hand) - self.strength(community_hand)
+        if(rel_strength == 0):
+            return 0
+        return 2**rel_strength
+        
+
+
     def is_garbage(self, own_hand, community_hand):
         return len(community_hand) > 0
+
+    def has_kings_aces(self,hand):
+        return any([card['rank'] for card in hand if card['rank'] in ['K','A']])
 
     def is_pair(self, hand):
         ranks = [card['rank'] for card in hand]
